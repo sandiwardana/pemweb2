7b129a3974b916259bb40a758ffcf3a22df95599
sandiwardana <sandygg5567@gmail.com>
2024-03-22 10:28:36 +0700

    Praktikum 5

---
+++ Praktikum 5/class_mahasiswa.php
@@ -0,0 +1,39 @@
+<?php
+
+class Mahasiswa {
+    // Property
+    public $ipk;
+    public $nama;
+    public $nim;
+    public $prodi;
+    public $angkatan;
+
+
+    // Method 
+    function __construct($_nim, $_nama){
+        $this->nim = $_nim;
+        $this->nama = $_nama;
+    }
+
+    function predikat_ipk(){
+        if($this->ipk < 2.0){
+            return 'CUKUP';
+        } elseif ($this->ipk >= 2.0 && $this->ipk < 3.0){
+            return 'BAIK';
+        } elseif ($this->ipk >= 2.0 && $this->ipk < 3.75){
+            return 'MEMUASKAN';
+        } elseif ($this->ipk >= 3.75 && $this->ipk <= 4.0){
+            return 'CUM LAUDE';
+        } else {
+            return 'NILAI DI LUAR JANGKAUAN';
+        }
+    }
+}
+
+// Instance Object
+$mahasiswa1 = new Mahasiswa(110223281, 'Sandy Wardana');
+$mahasiswa1->ipk = 3.75;
+$mahasiswa1->prodi = 'Teknik Informatika';
+$mahasiswa1->angkatan = 2023;
+?>
+
